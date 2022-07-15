f435811299e08479b7e525463740056636d4d0f5
aissat06 <youcefaissat7@gmail.com>
2022-07-15 22:23:05 +0200

    Creation de la classe Router pour gerer le routage Creation de fichier default.php a la place de header et footer avec l'utilisation des Fonctions de bufferisation de sortie installation de package var-dumper(symfony) pour avoir les methode 'dd' et 'dump'

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInit601c51f8a71e1e8530e4da781acad2e8
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit601c51f8a71e1e8530e4da781acad2e8::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire601c51f8a71e1e8530e4da781acad2e8($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire601c51f8a71e1e8530e4da781acad2e8($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
